1139d995d5b45e6243a35ec72747e2e127c2200c
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-10-04 12:03:12 +0200

    added different background color for odd sections portfolio in css, faded in my about me aka intro-message with jquery, to be able to bounce button on hover downloaded jquery UI, made first github button bounce

---
+++ includes/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('#social-icon').hover(function() {
+    $(this).effect( 'bounce', { times: 1 }, 'slow');
+
+  });
+});
+
+
+$('.intro-message').hide();
+$('.intro-message').fadeIn(7000);
