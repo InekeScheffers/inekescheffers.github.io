ed2a096c49f925487d2cf76ef8568b950330f7b5
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-10-04 16:08:26 +0200

    slowed down the scroll animation a bit in jquery

--- includes/main.js
+++ includes/main.js
@@ -13,6 +13,6 @@ $('.scroll').click(function(event){
 	event.preventDefault();
     $('html, body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+    }, 1250);
     return false;
 });
