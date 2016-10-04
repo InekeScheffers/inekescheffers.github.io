6b7fa8b13887482f14d49a48a907e3a86b5f75e8
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-10-04 15:16:23 +0200

    added smooth scroll with jquery to smoothly scroll to content when clicked in nav bar instead of jump

--- includes/main.js
+++ includes/main.js
@@ -8,3 +8,11 @@ $(document).ready(function() {
 
 $('.intro-message').hide();
 $('.intro-message').fadeIn(7000);
+
+$('.scroll').click(function(event){
+	event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+});
